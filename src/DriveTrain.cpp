82d297ab04b80c352ec59469a218c235941f62c2
Mikinator <ifyouno@gmail.com>
2015-01-08 23:59:07 -0500

    added ozDrive
    might have to play around with the x and y values because they might be getting mixed up

--- src/DriveTrain.cpp
+++ src/DriveTrain.cpp
@@ -16,3 +16,19 @@ DriveTrain::~DriveTrain()
     delete rearRight;
 }
 
+void DriveTrain :: ozDrive(Joystick* stick){
+//put the stick values in a couple floats
+float xVal =stick->GetX();
+float yVal= stick->GetY();
+
+//arcade drive
+float leftMotor = yVal + xVal;
+float rightMotor = yVal - xVal;
+
+frontLeft->Set(leftMotor);
+rearLeft->Set(leftMotor);
+
+frontRight->Set(rightMotor);
+rearRight->Set(rightMotor);
+
+}
