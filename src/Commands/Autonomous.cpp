884b8052cf7f773414d6c63d7b91a736de7a5eb4
Mikinator <ifyouno@gmail.com>
2015-01-19 17:30:46 -0500

    added Autonomous command group

---
+++ src/Commands/Autonomous.cpp
@@ -0,0 +1,16 @@
+#include "Autonomous.h"
+#include "CommandRackDoNothing.h"
+#include "CommandRackExtend.h"
+#include "CommandRackFlat.h"
+#include "CommandRackRaise.h"
+#include "CommandShovelDump.h"
+#include "CommandShovelGround.h"
+#include "CommandShovelHuman.h"
+#include "CommandShovelLift.h"
+#include "CommandShovelStore.h"
+
+#include <iostream>
+
+Autonomous::Autonomous() : CommandGroup("Autonomous") {
+	AddSequential(new CommandRackDoNothing())
+}
