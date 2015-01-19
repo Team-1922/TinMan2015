ecf35882ab9f554cae0a0c4fcf4c779c4a227d4a
Mikinator <ifyouno@gmail.com>
2015-01-19 18:58:05 -0500

    Added TestCommandGroup

---
+++ src/Commands/TestCommandGroup.h
@@ -0,0 +1,13 @@
+#ifndef TestCommandGroup_H
+#define TestCommandGroup_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class TestCommandGroup: public CommandGroup
+{
+public:
+	TestCommandGroup();
+};
+
+#endif
