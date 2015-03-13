79201d97f825e36e998fe5c521bd00ce6d52feee
KevinMackenzie <kjmack305@gmail.com>
2015-03-12 21:38:17 -0400

    bugfixes and tuning

--- src/Commands/PrepareHumanStation.cpp
+++ src/Commands/PrepareHumanStation.cpp
@@ -2,6 +2,7 @@
 #include "ShovelRotate.h"
 #include "RobotMap.h"
 #include "ShovelSetGrab.h"
+#include "ShovelSetCollect.h"
 
 PrepareHumanStation::PrepareHumanStation()
 {
@@ -10,5 +11,6 @@ PrepareHumanStation::PrepareHumanStation()
 	// even though that would probably never happen.
 
 	AddSequential(new ShovelSetGrab(false));
+	AddSequential(new ShovelSetCollect(false));
 	AddSequential(new ShovelRotate(RobotMap::Shovel::voltageFlat));
 }
